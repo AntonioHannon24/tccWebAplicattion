6c9e8f1801145ed746888a8dea10e793f1a8c4e7
AntonioHannon24 <antoniohannon24@gmail.com>
2023-11-05 21:02:34 -0300

    introdução a agemda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -43,6 +43,13 @@ import { FormContatoComponent } from './pages/formularios/contato/form-contato/f
 import { ListaContatosComponent } from './administrativo/lista-contatos/lista-contatos.component';
 import { FormContatosComponent } from './administrativo/formularios/form-contatos/form-contatos.component';
 import { EditContatosComponent } from './administrativo/edit-contatos/edit-contatos.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import {MatCardModule} from '@angular/material/card';
+
+import {MatNativeDateModule} from '@angular/material/core';
+
+
 
 @NgModule({
   declarations: [
@@ -89,7 +96,11 @@ import { EditContatosComponent } from './administrativo/edit-contatos/edit-conta
     AppRoutingModule,
     HttpClientModule,
     NgxPaginationModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatCardModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
